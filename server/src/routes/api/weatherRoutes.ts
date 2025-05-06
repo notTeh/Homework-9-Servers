9b3f5be54bc5d3dc376e9ad5ebdee8bdc95c10b4
Tyler Hayman <tyler.hayman@icloud.com>
2025-05-05 18:07:39 -0600

    Fixed errors

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -5,30 +5,30 @@ import HistoryService from '../../service/historyService.js';
 import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', async (req: Request, res: Response) => {
+router.post('/', (req: Request, res: Response) => {
   try {
     // Extract city name from request body
-    const { city } = req.body;
+    const cityName = req.body.cityName;
     
-    if (!city) {
-      return res.status(400).json({ error: 'City name is required' });
-    }
+    //if (!cityName) {
+      //return res.status(400).json({ error: 'City name is required' });
+    //}
 
   // TODO: GET weather data from city name
-  const weatherData = await WeatherService.getWeatherForCity(city);
-
+  //const weatherData = WeatherService.getWeatherForCity(cityName);
+  WeatherService.getWeatherForCity(cityName).then((weatherData) => {
   // TODO: save city to search history
-  await HistoryService.addCity(city);
-  res.status(200).json(weatherData);
-  }
-  catch (error) {
+  HistoryService.addCity(cityName);
+  return res.json(weatherData);
+  });
+  } catch (error) {
     console.error('Error fetching weather data:', error);
     res.status(500).json({ error: 'Failed to fetch weather data' });
   }
 });
 
 // TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {
+router.get('/history', async (_req: Request, res: Response) => {
   try {
     // Get search history from HistoryService
     const cities = await HistoryService.getCities();
@@ -44,6 +44,6 @@ router.get('/history', async (req: Request, res: Response) => {
 });
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req: Request, res: Response) => {});
+//router.delete('/history/:id', async (req: Request, res: Response) => {});
 
 export default router;
